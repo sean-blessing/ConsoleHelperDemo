370c7371a4860496ef776efc60b2dfb035dfd079
Sean Blessing <snblessing@gmail.com>
2018-03-22 16:02:08 -0400

    initial commit

---
+++ src/ConsoleHelperDemoApp.java
@@ -0,0 +1,20 @@
+import consolehelper.util.Console;
+
+public class ConsoleHelperDemoApp {
+
+	// single line comment
+	public static void main(String[] args) {
+		System.out.println("hello");
+		String str = Console.getString("Enter some text:  ");
+		System.out.println("You entered:  "+str);
+		System.out.println("bye");
+	}
+	/*
+	 * block comment
+	 */
+	
+	/**
+	 * JavaDoc
+	 */
+
+}
